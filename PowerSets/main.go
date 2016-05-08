fb1084cb9cb69c2f30c8c59bd34e9286c1ce7018
Marcin Majewski <majewskimarcin@outlook.com>
2016-05-08 02:43:21 +0200

    * Now input is trimmed

--- PowerSets/main.go
+++ PowerSets/main.go
@@ -1,15 +1,16 @@
-package PowerSets
+package main
 
 import (
 	"bufio"
 	"os"
 	"fmt"
+	"strings"
 )
 
 func main(){
 	reader := bufio.NewReader(os.Stdin);
-	receivedText, _ := reader.ReadString("\n")
-	allPowerSets := generateAllPowerSets(receivedText)
+	receivedText, _ := reader.ReadString('\n')
+	allPowerSets := generateAllPowerSets(trimString(receivedText))
 	for _, value := range allPowerSets{
 		fmt.Println(value)
 	}
@@ -17,5 +18,22 @@ func main(){
 
 func generateAllPowerSets(input string) map[string]bool{
 	powerSets := make(map[string]bool)
+	characters := make([]rune,0,0);
+	//lenOfString := len(input)
+	for _, r := range input{
+		characters = append(characters,r)
+	}
 
+	for _, value := range characters{
+		fmt.Println(string(value))
+	}
+
+	return powerSets
+}
+
+
+func trimString(stringToTrim string) string {
+	var trimmedString string
+	trimmedString = strings.TrimSpace(stringToTrim)
+	return trimmedString
 }
